e225c6bad283b063450d8d54c7e27b7b055fddf7
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-13 20:10:26 +0300

    переделываем каталог

--- app/app.view.ts
+++ app/app.view.ts
@@ -1,3 +1,9 @@
 namespace $.$$ {
-	export class $ds_app extends $.$ds_app {}
+	export class $ds_app extends $.$ds_app {
+		@$mol_mem
+		pages() {
+			const spread = this.spread_current()
+			return [this.Navbar(), ...(spread ? [spread] : [])]
+		}
+	}
 }
