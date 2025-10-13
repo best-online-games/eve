ad3a1540526a1e18799a09ad08c8f072c22dc5ed
zalesskiy.oleg <zalesskiy.oleg@wb.ru>
2025-10-13 19:34:54 +0300

    chore: draft surface компонента

--- themer/page/components/components.view.ts
+++ themer/page/components/components.view.ts
@@ -13,36 +13,53 @@ namespace $.$$ {
 			return titles[id] || id
 		}
 
-		// 5 строк - по одной на каждый размер
-		@$mol_mem
-		size_rows() {
-			return $ds_surface.SIZES.map(size => (this as any).Size_row(size))
-		}
-
-		// Колонки в строке - все цвета для данного размера
-		@$mol_mem_key
-		size_surfaces(size: string) {
-			return $ds_surface.COLORS.map(color => 
-				(this as any).Surface_cell(`${color}__${size}`)
-			)
-		}
-
-		@$mol_mem_key
-		surface_color(id: string) {
-			
-			return id.split('__')[0]
-		}
+	// Все ячейки grid: заголовки + данные
+	@$mol_mem
+	grid_cells() {
+		const cells: any[] = []
 		
-		@$mol_mem_key
-		surface_interactive(id: string) {
-			return id.split('__')[1]
-		}
+		// Первая строка: пустая ячейка + заголовки состояний
+		cells.push(this.Grid_empty())
+		$ds_surface.STATE_MODIFIERS.forEach(state => {
+			cells.push((this as any).Grid_header(state))
+		})
+		
+		// Строки данных: лейбл цвета + все состояния этого цвета
+		$ds_surface.COLORS.forEach(color => {
+			// Лейбл цвета
+			cells.push((this as any).Grid_row_label(color))
+			
+			// Все состояния для этого цвета
+			$ds_surface.STATE_MODIFIERS.forEach(state => {
+				cells.push((this as any).Grid_cell(`${color}__${state}`))
+			})
+		})
+		
+		return cells
+	}
 
-		// Текст лейбла
-		@$mol_mem_key
-		surface_label_text(id: string) {
-			const [color, size] = id.split('__')
-			return `${color}\n${size}`
-		}
+	// Текст заголовка состояния
+	@$mol_mem_key
+	grid_header_text(state: string) {
+		return state.toUpperCase()
+	}
+
+	// Текст лейбла цвета
+	@$mol_mem_key
+	grid_row_text(color: string) {
+		return color.toUpperCase()
+	}
+
+	// Создаём Surface по ID (color__state)
+	@$mol_mem_key
+	Grid_cell(id: string) {
+		const [color, state] = id.split('__')
+		
+		const surface = new this.$.$ds_surface()
+		surface.colors(color as any)
+		surface.state(state as any)
+		
+		return surface
+	}
 	}
 }
