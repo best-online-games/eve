d957f97af2b712d55821346e63deac65c030acc0
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-10-14 10:29:41 +0300

    нормальный навбар

--- app/app.view.ts
+++ app/app.view.ts
@@ -1,3 +1,7 @@
 namespace $.$$ {
+	/**
+	 * Main application component using mol_deck for tabbed page navigation
+	 * The deck automatically creates tab switcher from item titles
+	 */
 	export class $ds_app extends $.$ds_app {}
 }
