c2cc2ec69050311bfc3f326630460f7786b66912
Oleg Zalesskiy <ozalesskiy71@gmail.com>
2025-10-28 01:21:47 +0300

    вайбкодинг

--- app/page/sb/playground/playground.view.css.ts
+++ app/page/sb/playground/playground.view.css.ts
@@ -2,29 +2,51 @@ namespace $.$$ {
 	const { rem, px, vh } = $mol_style_unit
 
 	$mol_style_define( $eve_app_page_sb_playground, {
+		flex: {
+			grow: 1,
+		},
+
+		Demo_page: {
+			flex: {
+				basis: rem( 30 ),
+			},
+		},
+
+		Code_page: {
+			flex: {
+				basis: rem( 40 ),
+			},
+		},
 
 		Live_component_container: {
-			minHeight: '100%',
-			height: vh( 30 ),
-			maxHeight: vh( 80 ),
-			width: vh( 30 ),
-			maxWidth: vh( 80 ),
+			maxWidth: '100%',
+			minHeight: vh( 50),
+			maxHeight: vh( 50),
+			flex: {
+				grow: 1,
+			},
 		},
 
 		Editor_body: {
-			maxWidth: vh( 80 ),
-			width: 'fit-content',
-			height: vh( 30 ),
-			maxHeight: vh( 80 ),
-			padding: {
-				left: rem( 0.5 ),
-				right: rem( 1 ),
-			}
+			flex: {
+				grow: 1,
+			},
+			padding: rem( 1 ),
 		},
 
 		Source_text: {
 			width: '100%',
-		}
+			flex: {
+				grow: 1,
+			},
+			fontFamily: 'monospace',
+		},
+
+		Source_tree: {
+			flex: {
+				grow: 1,
+			},
+		},
 	} )
 }
 
