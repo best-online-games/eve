eb704b53d34bec6e1987a3afdc3d90a2979dae4a
Oleg Zalesskiy <ozalesskiy71@gmail.com>
2025-10-28 00:42:40 +0300

    update

--- app/page/sb/page/page.view.ts
+++ app/page/sb/page/page.view.ts
@@ -1,7 +1,6 @@
 namespace $.$$ {
 	export class $eve_app_page_sb_page extends $.$eve_app_page_sb_page {
 		
-		@$milis_log
 		title_lower(): string {
 			return this.title() ? this.title().replaceAll( ' ', '/' ).toLowerCase() : ''
 		}
