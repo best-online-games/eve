91b8451bf82680445633abd704d513b818f68318
Oleg Zalesskiy <ozalesskiy71@gmail.com>
2025-11-17 23:41:22 +0300

    tmp

---
+++ radio/group/group.view.ts
@@ -0,0 +1,16 @@
+namespace $.$$ {
+
+    export class $eve_radio_group extends $.$eve_radio_group {
+
+        options(): Record<string, string> { return {} }
+
+        value( next?: string ) { return next ?? '' }
+
+    }
+
+}
+
+
+
+
+
---
+++ selection/single/single.view.ts
@@ -0,0 +1,46 @@
+namespace $.$$ {
+
+	/**
+	 * Abstract base class for single-selection option groups.
+	 * Formerly $eve_options_single.
+	 */
+	export class $eve_selection_single extends $.$eve_selection_single {
+
+		options(): Record<string, string> {
+			return {}
+		}
+
+		@$mol_mem
+		value( next?: string ): string {
+			return next ?? ''
+		}
+
+		@$mol_mem
+		option_ids() {
+			return Object.keys( this.options() )
+		}
+
+		@$mol_mem_key
+		option_value( id: string ): string { return id }
+
+		@$mol_mem_key
+		option_label( id: string ): string { return this.options()[ id ] || id }
+
+		@$mol_mem_key
+		option_selected( id: string ): boolean { return this.value() === id }
+
+		@$mol_mem_key
+		option_click( id: string, event?: Event ) {
+			if( !event ) return null
+			this.value( id )
+			return event
+		}
+
+	}
+
+}
+
+
+
+
+
